7574570b606b321e61f8e3390ea3a2c4321440df
Luis Vazquez <laloedhi@gmail.com>
2019-10-16 13:16:05 -0500

    Esta es la version con animaciones

--- www/js/jestos.js
+++ www/js/jestos.js
@@ -1,18 +1,81 @@
-var app={
+var app = {
     inicio: function(){
+        this.iniciaBotones();
+        this.iniciaFastClick();
+        this.iniciaHammer();
+    },
+ 
+    iniciaFastClick: function(){
+        FastClick.attach(document.body);
+    },
+ 
+    iniciaBotones: function(){
         var btnClaro = document.querySelector('#claro');
         var btnObscuro = document.querySelector('#obscuro');
  
         btnClaro.addEventListener('click',this.ponloClaro, false);
-        btnObscuro.addEventListener('click', this.ponloBobscuro, false);
+        btnObscuro.addEventListener('click',this.ponloBobscuro, false);
+       
     },
+ 
+    iniciaHammer: function() {
+        var zona = document.getElementById('zona-gestos');
+        var hammertime = new Hammer(zona);
+ 
+        hammertime.get('pinch').set({enable: true});
+        hammertime.get('rotate').set({enable: true});
+ 
+        zona.addEventListener('webkitAnimationEnd',function(e){
+            zona.className = '';
+        })
+ 
+        hammertime.on('tap', function(ev){
+            document.querySelector('#info').innerHTML = ev.type+'!';
+            zona.className = 'tap';
+        });
+ 
+        hammertime.on('doubletap', function(ev){
+            document.querySelector('#info').innerHTML = ev.type+'!';
+            zona.className = 'doubletap';
+        });
+       
+        hammertime.on('press', function(ev){
+            document.querySelector('#info').innerHTML = ev.type+'!';
+            zona.className = 'press';
+        });
+       
+        hammertime.on('swipe', function(ev){
+            document.querySelector('#info').innerHTML = ev.type+'!';
+            var clase = undefined;
+            direccion = ev.direction;
+ 
+            if(direccion==4) clase='swipeD';
+            if(direccion==2) clase='swipeI';
+            console.log(ev.rotation);
+            zona.className = clase;
+        });
+ 
+        hammertime.on('rotate', function(ev){
+            document.querySelector('#info').innerHTML = ev.type+'!';
+           
+            if(ev.rotation > 10) zona.className='rotate-R';
+            if(ev.rotation < 0) zona.className='rotate-L';
+           
+        });
+       
+    },
+ 
     ponloClaro: function(){
         document.body.className = 'claro';
     },
     ponloBobscuro: function(){
         document.body.className = 'obscuro';
     },
-
-};
-
-app.inicio();
+ };
+ 
+ if('addEventListener' in document){
+    document.addEventListener('DOMContentLoaded', function(){
+        app.inicio();
+    }, false);
+ }
+ 
